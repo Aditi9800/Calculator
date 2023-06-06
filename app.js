05757e2cb18697f0b24b779508f05565536cad0f
aditi_sahu <87092070+Aditi9800@users.noreply.github.com>
2023-06-06 10:48:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// var a=document.getElementById("in").innerHTML;
+// console.log(a);
+console.log("ans");
+function equalfun(){
+    console.log("equal");
+    console.log(document.getElementById("in").innerText);
+}
